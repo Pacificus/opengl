34f0957c041a47ce373bc9f05ca80f7a52d5f23b
dima <greshchook@gmail.com>
2018-02-15 17:00:02 +0200

    16:59

--- opengl/rotate_kub/kub/main.cpp
+++ opengl/rotate_kub/kub/main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
-
+#include <GL/freeglut.h>
 using namespace std;
 
-int main()
+int main(int argc, char** argv)
 {
-    cout << "Hello World!" << endl;
+    glutInit(&argc, argv);
     return 0;
 }
 
