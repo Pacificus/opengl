0e22a19eebd9168de09bcfbc78facade4ed82519
dima <greshchook@gmail.com>
2018-02-15 17:10:10 +0200

    17:10

--- opengl/rotate_kub/kub/main.cpp
+++ opengl/rotate_kub/kub/main.cpp
@@ -2,9 +2,31 @@
 #include <GL/freeglut.h>
 using namespace std;
 
+struct Setings
+{
+    enum{
+        WIDTH  = 600,
+        HEIGHT = 600,
+        ALPHA  = 600
+    };
+} set;
+
 int main(int argc, char** argv)
 {
     glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
+    glutInitWindowPosition(300, 200);
+    glutInitWindowSize(set.WIDTH, set.HEIGHT);
+    glutCreateWindow("kub");
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(-1, 1, -1, 1, -1, 1);
+
+    glClearColor(0, 0, 0, 0);
+
+    glutMainLoop();
+
     return 0;
 }
 
